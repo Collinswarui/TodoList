cb1a6d86651d1dfa7004654ce4d45462e3a148ea
Collinswarui <waruiwanjiru47@gmail.com>
2024-08-07 09:46:51 +0300

    Edit aspects of the code

--- Server/index.js
+++ Server/index.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/todos', todoRouter);
 
-const port = 3000;
+const port = 3007;
 app.listen(
     port, () => console.log(`Server running on port ${port}`)
 );
